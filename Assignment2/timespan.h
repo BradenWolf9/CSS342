2e575bf1516a2ce85ae2eae689cf83fbeffbb6ba
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-04-25 14:00:00 -0700

    Update timespan.h

--- Assignment2/timespan.h
+++ Assignment2/timespan.h
@@ -1,26 +1,57 @@
 //
 // Created by Yusuf Pisan on 4/2/18.
 //
+// Adjusted by Braden Wolf on 4/22/21
+//
 
 #ifndef ASS2_TIMESPAN_H
 #define ASS2_TIMESPAN_H
 
 #include <iostream>
 
+
 using namespace std;
 
 class TimeSpan {
+  // insert 0:00:00
   friend ostream &operator<<(ostream &out, const TimeSpan &ts);
 
 public:
-  // explicit TimeSpan(int Hour = 0, int Minute = 0, int Second = 0);
+  // explicit constructor
   explicit TimeSpan(double hour = 0, double minute = 0, double second = 0);
 
+  // copy constructor
+  TimeSpan(const TimeSpan &copy);
+
+  // copy assignment operator
+  TimeSpan &operator=(const TimeSpan &copy);
+
+  // move constructor
+  TimeSpan(TimeSpan &&move) noexcept;
+
+  // move assignment operator
+  TimeSpan &operator=(TimeSpan &&move) noexcept;
+
+  // Simplify
+  void simplify();
+
+  // flips sign of each time category
+  void flipSign();
+
   // add
-  TimeSpan operator+(const TimeSpan &ts) const;
+  TimeSpan &operator+(const TimeSpan &ts);
 
   // subtract
-  TimeSpan operator-(const TimeSpan &ts) const;
+  TimeSpan &operator-(const TimeSpan &ts);
+
+  // add given TimeSpan to this TimeSpan
+  TimeSpan &operator+=(const TimeSpan &ts);
+
+  // subtract given TimeSpan from this TimeSpan
+  TimeSpan &operator-=(const TimeSpan &ts);
+
+  // multiply timespan by an unsigned number
+  TimeSpan &operator*(unsigned int number);
 
   // check equality
   bool operator==(const TimeSpan &ts) const;
@@ -28,11 +59,17 @@ public:
   // check if not equal
   bool operator!=(const TimeSpan &ts) const;
 
-  // multiply timespan by an unsigned number
-  TimeSpan operator*(unsigned int number) const;
+  // less than
+  bool operator<(const TimeSpan &ts) const;
 
-  // TODO(student)
-  // to add operator+=, operator-=, operator<, operator>, operator<=, operator>=
+  // greater than
+  bool operator>(const TimeSpan &ts) const;
+
+  // less than or equal to
+  bool operator<=(const TimeSpan &ts);
+
+  // greater than or equal to
+  bool operator>=(const TimeSpan &ts);
 
   // hour component of timespan
   int getHour() const;
@@ -46,7 +83,21 @@ public:
   // true if timespan is 0 or larger
   bool isPositive() const;
 
+  // destructor
+  ~TimeSpan();
+
 private:
+  double hour = 0;
+  double minute = 0;
+  double second = 0;
+  int totalSeconds = 0;
+  bool isNegative = false;
+  TimeSpan *tsSum = nullptr;
+  TimeSpan *tsSub = nullptr;
+  TimeSpan *tsProduct = nullptr;
+  bool less = false;
+  bool greater = false;
+
 };
 
 #endif // ASS2_TIMESPAN_H
