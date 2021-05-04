206d84099c26f7b495c07830fd225fbf0be892fa
BradenWolf9 <bwolf1@uw.edu>
2021-05-03 20:53:18 -0700

    renamed assignment 3

---
+++ Assignment3/maze.h
@@ -0,0 +1,39 @@
+// Author: Braden Wolf
+// Description: header file for maze class
+// Date: 5/3/21
+
+#ifndef ASS3_MAZE_H
+#define ASS3_MAZE_H
+
+#include <ostream>
+
+using namespace std;
+
+enum CELL { CLEAR, WALL, PATH, VISITED };
+
+class Maze {
+  // prints ASCII display of maze
+  friend ostream &operator<<(ostream &Out, const Maze &Maze);
+
+private:
+  const static int MAX_SIZE = 100;
+  char Field[MAX_SIZE][MAX_SIZE];
+  int Width, Height;
+  int ExitRow, ExitColumn;
+
+public:
+  // constructor
+  explicit Maze(const string &FileName);
+  // checks if given location has any markers
+  bool isClear(int Row, int Col) const;
+  // marks given location with a '*'
+  void markAsPath(int Row, int Col);
+  // marks given location with a '+'
+  void markAsVisited(int Row, int Col);
+  // returns exit row
+  int getExitRow() const;
+  // returns exit column
+  int getExitColumn() const;
+};
+
+#endif //ASS3_MAZE_H
