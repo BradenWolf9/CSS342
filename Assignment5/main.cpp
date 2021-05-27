1ca369c142fef5eed60c2119af47ebd3f4e26a67
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-05-27 10:53:53 -0700

    Create main.cpp

---
+++ Assignment5/main.cpp
@@ -0,0 +1,12 @@
+//Take single line argument (int n)
+//the program should create a TBST with Keytype being int
+//insert the numbers in non incremental order
+//that means the iterator should not use a stack (use thread links)
+//then make a copy of the tree
+//delete all even numbers from the copy
+//Lastly use an iterator to perform an INORDER traversal of each tree, outputting to std::cout
+#include <iostream>
+
+int main() {
+  
+}
